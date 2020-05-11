5bbe5b624d6b50909a24aba5bed0c7f95339e376
talfers <tayloralfers@gmail.com>
2020-05-11 16:00:34 -0600

    inital creation

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ app = Flask(__name__)
 
 # This packages allows for saving files to app dir
 import os
-from geopy.geocoders import GoogleV3
 import pandas as pd
-import folium
+
 # Set path to upload csv (path of current app dirnae)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-geolocator = GoogleV3(api_key='AIzaSyCYNaAhybO8b6cO08LuYNLs2LZsmKDpaXc', domain='maps.googleapis.com', user_agent="geofindr_v1")
 
 # Create get route, function to run on request
 @app.route('/')
@@ -30,41 +27,15 @@ def send_csv():
             filename = file.filename
             destination = "/".join([target, filename])
             file.save(destination)
-            df = pd.read_csv(destination,converters={'zip':str})
-            df.columns = ['id','address','city','state','zip']
-            latitudes = []
-            longitudes = []
-            m = folium.Map(location=[37, -95], tiles="OpenStreetMap", zoom_start=5)
-
-            for index, row in df.iterrows():
-                address = str(row['address'])
-                city = str(row['city'])
-                state = str(row['state'])
-                zip = str(row['zip'])
-                location = geolocator.geocode("{} {} {} {}".format(address, city, state, zip), timeout=10)
-                if location == None:
-                    latitudes.append('unknown')
-                    longitudes.append('unknown')
-                else:
-                    latitudes.append(location.latitude)
-                    longitudes.append(location.longitude)
-                    folium.Marker(
-                    location=[location.latitude, location.longitude],
-                    icon=folium.Icon(color='red')
-                    ).add_to(m)
-            df['latitude'] = latitudes
-            df['longitude'] = longitudes
-            m.save('templates/store_map.html')
+            df = pd.read_csv(destination)
+            print(df)
+            # df.columns = ['column_1','column_2','column_3','column_4','column_5']
             csvfinal = df
             os.remove(destination)
-            csvfinal.to_csv(target + '/updated_store_list.csv', index=False)
+            csvfinal.to_csv(target + '/updated_file.csv', index=False)
             return render_template('results.html')
     else:
             return redirect(url_for("home_route"))
-
-@app.route('/map')
-def map_route():
-    return render_template('store_map.html')
 
 # This runs the server (provided by Flask)
 if __name__ == '__main__':
